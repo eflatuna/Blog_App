6b05f5ce0d4dd961968b983a8c56ccf589142890
esra <esrabaskaya84@gmail.com>
2024-04-29 20:13:39 +0200

    Files created

---
+++ src/helper/ToastNotify.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToastNotify = () => {
+	return <div>ToastNotify</div>;
+};
+
+export default ToastNotify;
---
+++ src/hooks/useAuthCall.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const useAuthCall = () => {
+	return <div>useAuthCall</div>;
+};
+
+export default useAuthCall;
